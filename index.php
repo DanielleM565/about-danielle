4ecbde6bb369bb1de6dbcc7f2316086b5e809ad5
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-10 11:39:42 -0600

    Made first line body and big

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 	</head>
 	<body>
 		<p>
-			About me:
+			<stlye>
+		<h1>A little bit about me:</h1>
+			</stlye>
 		</p>
 		<p>Hello I am Danielle Isles Martin and I hope to be a professional problem solver. <br>
 			For a long time computers and all things technology have caused me more problems than solved. <br>
@@ -18,15 +20,17 @@
 		<img src="images/danielle.img.jpg" alt="image of me" height="250" width="250"/>
 
 		<p> I graduated in 2015 from University of New Mexico with my Bachelor's of Science in Biochemisty.
-			<br>Since Graduation I have been taking time to enjoy the things I love. </p>
-<p>
-			Here is a list of things I love
+			<br>Since Graduation I have been taking time to enjoy the things I love.
+			<br>Here is a list of things I love
 		<ul>
 			<li>Movies</li>
-			<li>Sleeping</li>
+			<li>Photography</li>
 			<li>Jogging</li>
 			<li>Traveling</li>
+			<li>Gaming</li>
+			<li>Gaming</li>
 		</ul>
+
 		</p>
 	</body>
 </html>
