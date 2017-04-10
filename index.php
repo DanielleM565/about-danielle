09d29030ff6b05226c362dfdb29cc51296f3b766
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-10 11:18:15 -0600

    created about me page with 2 paragraphs and one big image

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Danielle Martin
+		</title>
+	</head>
+	<body>
+		<p>
+			About me:
+		</p>
+		<p>Hello I am Danielle Isles Martin and I hope to be a professional problem solver. <br>
+			For a long time computers and all things technology have caused me more problems than solved. <br>
+			In this new age of technology I no longer wish to be ignorant and learn how IT works<br>
+			(get it? I.T.) <br>
+			I hope to use the new skills I will gain here to start my new career solving problems… <br>
+			most of them my own. </p>
+		<img src="images/danielle.img.jpg" alt="image of me"/>
+
+		<p> I graduated in 2015 from University of New Mexico with my Bachlor's of Science in Biochemisty.</p>
+	</body>
+</html>
